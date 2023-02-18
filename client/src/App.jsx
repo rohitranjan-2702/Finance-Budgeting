cc54d7002b4b3f680ebea941731b1b98726b4d1a
Rohit <rohiteditz.mail@gmail.com>
2023-02-19 01:09:20 +0530

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (<>
     <div className="App">
       
     <Home/>
+    <Footer/>
     </div>
     </>
   );
