b9cce23210b373835ef13000342c6cdff43f4287
Rohit <rohiteditz.mail@gmail.com>
2023-02-19 12:22:16 +0530

    expense card added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,22 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ExpenseCard from "./components/ExpenseCard";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <>
+    <ChakraProvider>
+      
       <div className="App">
         <Home />
+        <ExpenseCard/>
         <Login />
         <Signup />
         <Footer />
       </div>
+    </ChakraProvider>
     </>
   );
 }
