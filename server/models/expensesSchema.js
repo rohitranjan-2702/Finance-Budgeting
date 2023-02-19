0f25f2cdd67863e1dbdecc5699061fa1002d7e3b
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-02-19 15:14:10 +0530

    expense api working

--- server/models/expensesSchema.js
+++ server/models/expensesSchema.js
@@ -1,36 +1,42 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-
-const expenses=new Schema({
-    date:{
-        type:String,
-        required:true,
+const expenses = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  tags: [
+    {
+      type: String,
+      required: true,
     },
-    amount:{
-        type:Number,
-        required:true,
-    },
-    tag:{
-        type:String,
-        required:true
-    }
-
-})
-const expensesSchema=new Schema({
-    year:{
-
-        type:Number,
-        required:true,
-        min:4
-    },
-    month:{
-        type:String,
-        required:true,
-        min:3,
-        max:12
-    },
-    expenses:[expenses]
-
-})
-module.exports=mongoose.model('expensesamount',expensesSchema);
+  ],
+});
+const expensesSchema = new Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+    min: 4,
+  },
+  month: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 12,
+  },
+  expenses: [expenses],
+});
+module.exports = mongoose.model("expensesamount", expensesSchema);
