6434b984b05861f5dfc4279be1eb2f04cdd79ad0
Rohit <rohiteditz.mail@gmail.com>
2023-02-19 19:53:10 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,12 @@ import Home from "./pages/home";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import ExpenseCard from "./components/ExpenseCard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/header";
-<<<<<<< HEAD
 import Expense from "./pages/Expense";
 
-=======
->>>>>>> ba9aba8f0f2a19f5c94bd96f5d0b9cab1ff4e2cd
 function App() {
   return (
     <ChakraProvider>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import ExpenseChart from '../components/ExpenseChart'
 import MonthlyExpense from '../components/MonthlyExpense'
 
 
 const Dashboard = () => {
   return (<>
-    <ExpenseChart/>
-    <MonthlyExpense />
+    <div class="bg-slate-200">
+        <div class="m-0 flex max-w-full flex-col items-center rounded-md bg-slate-200">
+            <div class="mb-2 mt-4 text-3xl font-bold">Dashboard</div>
+            <div class="mb-4 text-2xl font-semibold">Hello 🖐, Welcome<span class="text-blue-500"> Rohit !</span></div>
+        </div>
+        <div class="flex md:flex-row flex-col">
+            <div class=" flex h-[250px] md:w-1/2 w-screen ">
+            <MonthlyExpense />
+            </div>
+            <div class="m-4 flex md:w-1/2 w-full flex-col items-center justify-center p-4">
+            <div class="mb-4 text-3xl">Income: <span class="text-4xl font-bold text-blue-600">$7000</span></div>
+            <div class=" text-3xl">Expenses: <span class="text-4xl font-bold text-red-600">$7000</span></div>
+            </div>
+        </div>
+        <div class="flex items-center justify-center md:m-4">
+            <div class="mb-4 text-3xl">Your Savings: <span class="text-4xl font-bold text-green-600">$3000</span></div>
+        </div>
+    </div>
     </>
   )
 }
